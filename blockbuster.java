d704ce5b645f2d25b3e39b7568cc750381ad00b8
talifrederick <talimfrederick@yahoo.com>
2022-12-09 08:55:08 -0600

    first

---
+++ blockbuster.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class blockbuster{
+
+    static Scanner sc = new Scanner(System.in);
+
+    
+
+
+
+}
---
+++ videogames.java
@@ -0,0 +1,3 @@
+public class videogames extends rentableitems {
+    
+}
